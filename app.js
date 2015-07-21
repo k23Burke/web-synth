03e979f31ba846cef0eb997509c948a521771267
k23Burke <k23burke@gmail.com>
2015-07-20 22:58:01 -0400

    tone now local

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.get('/css/main.css', function (req, res, next) {
 app.get('/js/midiscratch.js', function (req, res, next) {
 	res.sendFile(__dirname + '/js/midiscratch.js');
 });
+app.get('/js/tone.min.js', function (req, res, next) {
+	res.sendFile(__dirname + '/js/tone.min.js');
+});
 
 app.get('favicon.ico', function (req, res, next) {
 	res.sendFile(__dirname + '/img/main.css');
